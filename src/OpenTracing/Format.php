f709b747e4ba883106edb836a615daf85dc8ee3f
Władysław Bodzek <wladek@wikia-inc.com>
2016-02-22 13:13:32 +0100

    Record @todo about carrier descriptions

--- src/OpenTracing/Format.php
+++ src/OpenTracing/Format.php
@@ -5,6 +5,8 @@ namespace OpenTracing;
 class Format
 {
 
+    // @todo: add more info about supported carriers for the builtin formats
+
     const SPLIT_TEXT = 'split_text';
     const SPLIT_BINARY = 'split_binary';
 
