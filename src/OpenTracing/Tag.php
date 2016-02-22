e13689dc51c213aa6fcf64aff050acb1c4815cf0
Władysław Bodzek <wladek@wikia-inc.com>
2016-02-22 14:40:58 +0100

    Add builtin tags

---
+++ src/OpenTracing/Tag.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OpenTracing;
+
+class Tag
+{
+
+    const PEER_SERVICE = "peer.service";
+    const PEER_HOSTNAME = "peer.hostname";
+    const PEER_HOST_IPV4 = "peer.ipv4";
+    const PEER_HOST_IPV6 = "peer.ipv6";
+    const PEER_PORT = "peer.port";
+    const SAMPLING_PRIORITY = "sampling.priority";
+
+}
